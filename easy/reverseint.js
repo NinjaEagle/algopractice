167756f779e4e90d2a2c9ba5c7b7dd604db6bc24
NinjaEagle <kcwang06@gmail.com>
2019-10-29 17:07:07 -0400

    reverse 32bit integer

--- easy/reverseint.js
+++ easy/reverseint.js
@@ -1,5 +1,17 @@
 // Given a 32-bit signed integer, reverse digits of an integer.
 
 var reverse = function(x) {
-    
+    //    turn to string
+    //    turn to array
+    //    reverse array
+    let isNegativeNum = false;
+    if (x<0){
+        isNegativeNum = true;
+        x = x *-1; 
+    }
+    let numString = (x +"").split("").reverse().join("")
+    let result = Number(numString);
+    if(result > 2**31) {
+        return 0}
+    return isNegativeNum ? result* -1 : result
 };
