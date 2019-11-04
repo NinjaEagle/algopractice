7619ba1806dbdd735648aa16d4beba2e91df7043
NinjaEagle <kcwang06@gmail.com>
2019-11-03 22:36:38 -0500

    organized folder structure

--- codewars/oddint.js
+++ codewars/oddint.js
@@ -1,3 +1,5 @@
+// Codewars Find the Odd Int 6 kyu problem 
+// https://www.codewars.com/kata/find-the-odd-int/train/javascript
 // Given an array, find the int that appears an odd number of times.
 
 // There will always be only one integer that appears an odd number of times.
