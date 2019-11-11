497d65a8b8406b540cb98b9f4be41cff962f8bd3
NinjaEagle <kcwang06@gmail.com>
2019-11-11 12:21:50 -0500

    pseudocode for pascals

--- leetcode/pascals.js
+++ leetcode/pascals.js
@@ -5,14 +5,19 @@
 
 var generate = function(numRows) {
 
+    // first row of result array
     let result=[[1]]
+    // it there are no rows
     if (numRows===0) return [];
+    //condition if there is a row
     if (numRows===1) return result;
     result.push([1,1])
+    // 2 rows condition
     if (numRows===2) return result
-    
-    for (let i=2;i<numRows ;i++){
+    // for loop i starting from 2 up to the total num or rows
+    for (let i=2; i<numRows; i++){
         let last=result[result.length-1]
+        // creating a temp array to form a new row after the current row
         let temp=[1]
        for(let j=0; j< last.length-1 ;j++){
            let sum=parseInt(last[j])+ parseInt(last[j+1])
