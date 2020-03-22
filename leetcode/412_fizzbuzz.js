2d709fbd851f62f9d3814983fd985c4837453236
Kevin Wang <kcwang06@gmail.com>
2020-03-22 17:59:34 -0400

    prob 412 leetcode

---
+++ leetcode/412_fizzbuzz.js
@@ -0,0 +1,13 @@
+// 412 leetcode https://leetcode.com/problems/fizz-buzz/
+var fizzBuzz = function(n) {
+	let arr = [];
+	for (let i = 1; i <= n; i++) {
+		if (i % 15 == 0) arr.push('FizzBuzz');
+		else if (i % 3 == 0) arr.push('Fizz');
+		else if (i % 5 == 0) arr.push('Buzz');
+		else {
+			arr.push(i.toString());
+		}
+	}
+	return arr;
+};
