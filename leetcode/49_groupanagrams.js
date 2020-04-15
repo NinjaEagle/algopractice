b98e8c2dee4011803cdb378db91c5ed4934fdd0d
Kevin Wang <kcwang06@gmail.com>
2020-04-15 14:41:36 -0400

    leetcode 49 solution

---
+++ leetcode/49_groupanagrams.js
@@ -0,0 +1,18 @@
+// 49. Group Anagrams https://leetcode.com/problems/group-anagrams/
+const groupAnagrams = (strs) => {
+	//     input strings in an array
+	//     ouput is an array with arrays of anagrams
+	if (strs.length === 0) return []
+	if (strs.length === 1) return [strs]
+	let myMap = new Map()
+
+	for (let i = 0; i < strs.length; i++) {
+		//use the spread operator on an array, sort the string and then convert it to string
+		const currKey = [...strs[i]].sort().toString()
+
+		if (myMap.has(currKey)) myMap.set(currKey, [...myMap.get(currKey), strs[i]])
+		if (!myMap.has(currKey)) myMap.set(currKey, [strs[i]])
+	}
+
+	return [...myMap.values()]
+}
